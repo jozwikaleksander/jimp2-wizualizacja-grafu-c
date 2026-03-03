482563cbd0f6244bfdded1ca850f4405c1531887
0-Cuprum-0 <imaf77198@gmail.com>
2026-03-03 19:32:07 +0100

    dodawanie plików do eades

---
+++ src/eades.c
@@ -0,0 +1,4 @@
+#include "graph.h"
+#include <string.h>
+#include <stdio.h>
+
--- src/eades.h
+++ src/eades.h
@@ -2,6 +2,11 @@
 #ifndef _EADES_H_
 #define _EADES_H_
 
+#define MINIMUM_FORCE 0.1
+
+void eades_algorithm()
+
+
 
 #endif
 
